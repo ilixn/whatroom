190ff7b9b59e68e0d52228ac1c1384112cd9da18
Hell0w <helloosong@gmail.com>
2018-06-18 00:02:54 +0200

    Rooms can be occuped (to finish), user can see which are occuped

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require('express');
+const fs = require('fs');
+
 var 
 	app = express();
-	verbose = 1;
 
 //Default homepage
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-//Serve any file
-app.get('/*' , function(req, res, next) {
-    var file = req.params[0];
-    if(verbose) console.log('\t :: Express :: file requested : ' + file);
+//List of empty/occuped rooms
+app.get('/salles.json' , function(req, res, next) {
+    var file = "salles.json"
     res.sendFile(__dirname + '/' + file);
 });
 
+//Change the state of a room
+app.get('/change', function(req, res, next) {
+	var room_nb = req.query.room;
+	res.send('OK!' + room_nb);
+	console.log(room_nb);
+	setRoomOccuped(room_nb);
+});
+
 app.listen(1234, () => console.log('Whatroom app listening on port 1234!'));
+
+function setRoomOccuped(id) {
+	fs.readFile('salles.json', function(err, data) {
+		if (err) throw err;
+		salles_list = JSON.parse(data);
+		salles_list.salles[id].state = false;
+		console.log(JSON.stringify(salles_list));
+		var salles_list_stringified = JSON.stringify(salles_list);
+		//fs.writeFile('salles.json', salles_list_stringified, (err) => { if (err) throw err } );
+	});
+}
