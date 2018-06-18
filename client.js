2aa57cf213de8e770b862c678a16841337393192
Hell0w <helloosong@gmail.com>
2018-06-18 13:41:16 +0200

    Basic socket.io connection

---
+++ client.js
@@ -0,0 +1,8 @@
+console.log("Client.js imported !");
+
+var socket = io.connect('/');
+
+socket.on('onconnected', function(data) {
+    //Note that the data is the object we sent from the server, as is. So we can assume its id exists.
+    console.log( 'Connected ! data.test:' + data.test);
+});
--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const express = require('express');
-const fs = require('fs');
+const
+  express = require('express'),
+  io = require('socket.io'),
+  http = require('http');
+var
+  app = express(),
+  port = 1234;
 
-var app = express();
+
 
 //Default homepage
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-//List of empty/occuped rooms
-app.get('/salles.json' , function(req, res, next) {
-    var file = "salles.json"
-    res.sendFile(__dirname + '/' + file);
+app.get('/*' , function(req, res, next) {
+  var file = req.params[0];
+  console.log('\t :: Express :: file requested : ' + file);
+  res.sendFile(__dirname + '/' + file);
 });
 
-//Change the state of a room
-app.get('/change', function(req, res, next) {
-	var room_nb = req.query.room;
-	var status = 'OK! Room: ' + room_nb;
-	res.send(status);
-	console.log(status);
-	setRoomOccuped(room_nb);
-});
 
-app.listen(1234, () => console.log('Whatroom app listening on port 1234!'));
 
-function setRoomOccuped(id) {
-  	var salles_list_stringified;
-	fs.readFile('salles.json', function(err, data) {
-		if (err) throw err;
-		salles_list = JSON.parse(data);
-		salles_list.salles[id].state = false;
-		console.log(JSON.stringify(salles_list));
-		salles_list_stringified = JSON.stringify(salles_list, null, 2);
-		fs.writeFile('salles.json', salles_list_stringified, (err) => { if (err) throw err } );
-      	//PROBLEM: WRITING IN AN OPEN FILE, CORRECTION : WRITE AFTER (TO TRY)
-	});
-  	//fs.writeFile('salles.json', salles_list_stringified, (err) => { if (err) throw err } );
-}
+//Socket.io setup
+var server = http.Server(app);
+
+var sio = io.listen(server);
+server.listen(port);
+
+sio.sockets.on('connection', function(client) {
+  client.emit('onconnected', { test: "value" });
+  console.log('\t socket.io:: client connected');
+
+  //Disconnection
+  client.on('disconnect', function () {
+      console.log('\t socket.io:: client disconnected');
+  });
+})
